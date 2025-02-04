69fd2d805b368281eb5a13f85a43c22313108dab
m4cren <156091605+m4cren@users.noreply.github.com>
2025-02-04 17:34:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from website import create_website, socketio, ip_address
+from website import create_website, socketio, ip_address, db
 
 
 app = create_website()
-
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+        db.session.remove()
 if __name__ == '__main__':
      socketio.run(app,  host = '0.0.0.0')
