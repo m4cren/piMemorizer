f13dad398d3368c1cbfcbc442c43c1d4fa90e2d9
m4cren <renrenofficial@gmail.com>
2025-02-04 20:39:29 +0800

    Resolved merge conflict in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from website import create_website, socketio, ip_address, db
 
 
 app = create_website()
-<<<<<<< HEAD
+
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
@@ -10,10 +10,6 @@ def shutdown_session(exception=None):
 
 if __name__ == '__main__':
      socketio.run(app, host='0.0.0.0')
-=======
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-        db.session.remove()
-if __name__ == '__main__':
-     socketio.run(app,  host = '0.0.0.0')
->>>>>>> 79d7d863f63b152934eeb06418f2775033f8b749
+
+
+
