bde2bad9f3a1eac85ba52df2923ac7afb1b2e5fa
m4cren <156091605+m4cren@users.noreply.github.com>
2025-02-06 00:39:18 +0800

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,6 @@
 import eventlet
 eventlet.monkey_patch()
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> e4a9f2e (New Versione)
 
 from flask import Flask
 from .extensions import db, migrate, login_manager, socketio, ip_address
@@ -26,29 +21,21 @@ def create_website():
 
      app = Flask(__name__)
      app.config['SECRET_KEY'] = 'secret_98479186421642961464692146216321'
-<<<<<<< HEAD
-=======
-     # app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:%40%23OctObEr102704@{ip_address}/pigame2?charset=utf8'
->>>>>>> e4a9f2e (New Versione)
+
      app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
      app.config['SQLALCHEMY_POOL_SIZE'] = 10 
      app.config['SQLALCHEMY_MAX_OVERFLOW'] = 20  
      app.config['SQLALCHEMY_POOL_RECYCLE'] = 3600  
-<<<<<<< HEAD
 
-=======
-   
->>>>>>> e4a9f2e (New Versione)
      
      db.init_app(app)
      migrate.init_app(app, db)
      socketio.init_app(app, async_mode='eventlet')
-<<<<<<< HEAD
 
 
 
-=======
->>>>>>> e4a9f2e (New Versione)
+
+
 
      from .auth import auth
      from .views import views
@@ -71,10 +58,7 @@ def create_website():
      def load_user(id):
           return User.query.get(int(id)) 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> e4a9f2e (New Versione)
      return app
 
 def create_database(app):
